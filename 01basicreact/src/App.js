78b03e68105931cb858effde0cfe0f26b5acf8b6
SuyashGautam05 <gautamsuyash5@gmail.com>
2024-05-24 17:04:17 +0530

    Created two file from create react app and vite

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+<h1>Suyash Gautam</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+<h1>Chai aur code with vite</h1>
+  )
+}
+
+export default App
