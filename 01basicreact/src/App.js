8d1fef4e42a4afb3f4dc780f7e9e319c755f7c01
SuyashGautam05 <gautamsuyash5@gmail.com>
2024-05-25 10:38:34 +0530

    Understanding the react.js

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,7 +1,10 @@
-
+import Chai from "./chai"
 function App() {
   return (
+    <>
+    <Chai/>
 <h1>Suyash Gautam</h1>
+</>
   );
 }
 
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,8 @@
+import Chai from "./chai" 
 
 function App() {
   return (
-<h1>Chai aur code with vite</h1>
-  )
+<Chai/>  )
 }
 
 export default App
